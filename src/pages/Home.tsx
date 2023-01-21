41fc20372adabc2a4a4bd5529d34d69a4b63b4f0
Md Nayem Hossain <nayemhossainmd1@gmail.com>
2023-01-21 19:30:11 +0600

    tailwind and flowbit installed

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <h2>Home Page</h2>
+            <h2 className="text-3xl font-bold underline">Home Page</h2>
         </div>
     );
 };
